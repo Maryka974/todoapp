69c86136c518456f2c4d085c5cec3ced636b1677
Maryka974 <marykajames@gmail.com>
2019-10-21 17:00:23 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$_POST
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<style type="text/css"></style>
+	<title> To do list</title>
+</head>
+<body>
+	<border> 
+
+<ul>
+	<li> Faire une app </li><br>
+	<li> Manger des chips</li><br>
+	<li> Cocher des trucks </li><br>
+</ul>
+
+</border>
+
+</body>
+</html>
